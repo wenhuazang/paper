400d31b3470ebb3fcf91ec0967704e8047c6159a
wenhuazang <wenhuazang@sohu-inc.com>
2017-11-28 20:11:01 +0800

    text_v3

--- model_text.py
+++ model_text.py
@@ -9,9 +9,6 @@ import tensorflow.contrib.slim as slim
 from datetime import datetime
 import os.path
 import time
-import numpy as np
-from flip_gradient import flip_gradient
-import math
 import params
 
 
@@ -33,73 +30,6 @@ class DANN(object):
         self.alpha = config.alpha
         self.mode = config.mode
 
-    def dan_s(self, inputs, labels, reuse=False):
-        with tf.variable_scope('dan_s', reuse=reuse):
-            with slim.arg_scope([slim.fully_connected],
-                                weights_initializer=tf.truncated_normal_initializer(stddev=0.02), activation_fn=None,
-                                weights_regularizer=slim.l2_regularizer(0.0001)):
-                with slim.arg_scope([slim.batch_norm], decay=0.80, center=True, scale=True, epsilon=1e-5,
-                                    activation_fn=tf.nn.relu, is_training=True):
-                    avg = tf.reduce_mean(inputs, axis=0, keep_dims=True)
-                    # avg_1 = tf.reduce_mean(inputs[int(self.batch_size):], axis=0, keep_dims=True)
-                    # avg = tf.concat([avg_0, avg_1], 0)
-                    #
-                    net = slim.fully_connected(avg, params.d_f0, scope="fc0")
-                    net = slim.dropout(net, params.keep_prob)
-                    net = slim.batch_norm(net, scope='bn0')
-                    # [4+2 -> 4]
-                    net = slim.fully_connected(net, params.d_f1, scope="fc1")
-                    net = slim.dropout(net, params.keep_prob)
-                    net = slim.batch_norm(net, scope='bn1')
-                    # [4+2 -> 4]
-                    net = slim.fully_connected(net, params.d_f2, scope="fc2")
-                    net = slim.batch_norm(net, scope='bn2')
-                    # [100]---[1]
-                    logits = slim.fully_connected(net, 1, scope="fc")
-
-                    return logits
-
-    def dan_s2(self, inputs, global_step):
-        with tf.variable_scope('dan_s2'):
-            with slim.arg_scope([slim.fully_connected],
-                                weights_initializer=tf.truncated_normal_initializer(stddev=0.02), activation_fn=None,
-                                weights_regularizer=slim.l2_regularizer(0.0001)):
-                with slim.arg_scope([slim.batch_norm], decay=0.80, center=True, scale=True, epsilon=1e-5,
-                                    activation_fn=tf.nn.relu, is_training=True):
-                    # avg_0 = tf.reduce_mean(feature[:int(self.batch_size)], axis=0, keep_dims=True)
-                    # avg_1 = tf.reduce_mean(feature[int(self.batch_size):], axis=0, keep_dims=True)
-                    # avg = tf.concat([avg_0, avg_1], 0)
-                    avgs = []
-                    for i in range(4):
-                        avgs.append(tf.reduce_mean(
-                            inputs[int(i * self.batch_size / 2):int((i + 1) * self.batch_size / 2)],
-                            axis=0, keep_dims=True))
-                    avg_00 = tf.abs(avgs[0] - avgs[1])
-                    avg_11 = tf.abs(avgs[2] - avgs[3])
-                    avg_01 = tf.abs(avgs[0] - avgs[2])
-                    avg_10 = tf.abs(avgs[1] - avgs[3])
-                    avg = tf.concat([avg_00, avg_11, avg_01, avg_10], 0)
-                    # [2500]---[128]
-                    net = slim.fully_connected(avg, params.d_f0, scope="fc0")
-                    net = slim.batch_norm(net, scope='bn0')
-
-                    # [128]---[1]
-                    logits = slim.fully_connected(net, 1, scope="fc1")
-                    source_logits = tf.slice(logits, [0, 0], [2, 1])
-                    target_logits = tf.slice(logits, [2, 0], [2, 1])
-
-                    # compute domain_loss
-                    s_source_domain_loss = tf.reduce_mean(
-                        tf.nn.sigmoid_cross_entropy_with_logits(
-                            logits=source_logits, labels=tf.ones_like(source_logits)
-                        ))
-                    s_target_domain_loss = tf.reduce_mean(
-                        tf.nn.sigmoid_cross_entropy_with_logits(
-                            logits=target_logits, labels=tf.zeros_like(target_logits)
-                        ))
-                    s_domain_loss = (s_source_domain_loss + s_target_domain_loss) / 2.0
-                    return s_domain_loss
-
     def kron(self, f_1, dim_1, f_2, dim_2):
         stack_1 = tf.stack([f_1 for _ in range(dim_1)], axis=2)
         stack_1 = tf.reshape(stack_1, [-1, dim_1 * dim_2])
@@ -110,21 +40,9 @@ class DANN(object):
 
     def generator(self, inputs, reuse=False):
         with tf.variable_scope("generator", reuse=reuse):
-            with slim.arg_scope([slim.conv2d, slim.fully_connected],
+            with slim.arg_scope([slim.fully_connected],
                                 weights_initializer=tf.truncated_normal_initializer(stddev=0.02), activation_fn=None,
                                 weights_regularizer=slim.l2_regularizer(0.001)):
-                with slim.arg_scope([slim.conv2d], kernel_size=[5, 5], stride=1, padding="SAME"):
-                    with slim.arg_scope([slim.max_pool2d], kernel_size=[2, 2], stride=2, padding="SAME"):
-                        with slim.arg_scope([slim.batch_norm], decay=0.80, center=True, scale=True, epsilon=1e-5,
-                                            activation_fn=tf.nn.relu, is_training=True):
-                            # net = slim.fully_connected(inputs, 1000, scope="fc0")
-                            # net = slim.batch_norm(net, scope='bn0')
-                            # # [1000]---[100]
-                            # net = slim.fully_connected(inputs, 100, scope="fc1")
-                            # net = slim.batch_norm(net, scope='bn1')
-                            # # [100]---[2] no activation function
-                            # net = slim.fully_connected(net, 2, scope="fc2")
-                            ''''''
                             net = slim.fully_connected(inputs, 50, scope="fc0")
                             net = tf.nn.relu(net)
                             return net
@@ -134,13 +52,6 @@ class DANN(object):
             with slim.arg_scope([slim.fully_connected],
                                 weights_initializer=tf.truncated_normal_initializer(stddev=0.02), activation_fn=None,
                                 weights_regularizer=slim.l2_regularizer(0.001)):
-                with slim.arg_scope([slim.batch_norm], decay=0.80, center=True, scale=True, epsilon=1e-5,
-                                    activation_fn=tf.nn.relu, is_training=True):
-                    # net = slim.fully_connected(inputs, 2, scope="fc0")
-                    # net = slim.batch_norm(net, scope='bn0')
-                    # logits = slim.fully_connected(net, 2, scope="fc1")
-                    # logits = tf.add(inputs, logits)
-                    ''''''
                     net = slim.fully_connected(inputs, 2, scope="fc0")
                     return net
 
@@ -149,35 +60,14 @@ class DANN(object):
             with slim.arg_scope([slim.fully_connected],
                                 weights_initializer=tf.truncated_normal_initializer(stddev=0.02), activation_fn=None,
                                 weights_regularizer=slim.l2_regularizer(0.0001)):
-                with slim.arg_scope([slim.batch_norm], decay=0.80, center=True, scale=True, epsilon=1e-5,
-                                    activation_fn=tf.nn.relu, is_training=True):
                     if labels is None:
-                        # net = slim.fully_connected(inputs, 50, scope="fc0")
-                        # net = slim.batch_norm(net, scope='bn0')
-                        # net = slim.fully_connected(net, params.d_f1, scope="fc1")
-                        # net = slim.batch_norm(net, scope='bn1')
-                        # # [100]---[1]
-                        # logits = slim.fully_connected(net, 1, scope="fc")
                         logits = slim.fully_connected(inputs, 1, scope="fc0")
-                        # logits = slim.fully_connected(net, 1, scope="fc")
                     else:
-                        # [4+2 -> 4]
-                        # net = slim.fully_connected(tf.concat([inputs, labels], axis=1), params.d_f0, scope="fc0")
-                        # net = slim.dropout(net, params.keep_prob)
-                        # net = slim.batch_norm(net, scope='bn0')
-                        # # [4+2 -> 4]
-                        # net = slim.fully_connected(tf.concat([net, labels], axis=1), params.d_f1, scope="fc1")
-                        # net = slim.dropout(net, params.keep_prob)
-                        # net = slim.batch_norm(net, scope='bn1')
-                        # # [4+2 -> 4]
-                        # net = slim.fully_connected(tf.concat([net, labels], axis=1), params.d_f2, scope="fc2")
-                        # net = slim.batch_norm(net, scope='bn2')
-                        # [100]---[1]
                         logits = slim.fully_connected(tf.concat([inputs, labels], axis=1), 1, scope="fc")
                     return logits
 
     def build_model(self, source_images, target_images, source_labels,
-                    target_labels, step, mode, is_gan, is_train=False):
+                    target_labels, step, mode, is_train=False):
         if not is_train:
             target_feature = self.generator(target_images)
             target_logits = self.classifier(target_feature)
@@ -187,19 +77,14 @@ class DANN(object):
         # if seperate generator with reuse, acc down!!! So, merge it... maybe the parameter step?
         merged_images = tf.concat([source_images, target_images], 0)
         merge_features = self.generator(merged_images)
-        # source_feature = tf.slice(merege_features, [0, 0], [self.batch_size, params.g_last])
-        # target_feature = tf.slice(merege_features, [self.batch_size, 0], [self.batch_size, params.g_last])
+        # source_feature = tf.slice(merge_features, [0, 0], [self.batch_size, params.g_last])
+        # target_feature = tf.slice(merge_features, [self.batch_size, 0], [self.batch_size, params.g_last])
         # source_logits = self.classifier(source_feature)
         # target_logits = target_feature
         c_logits = self.classifier(merge_features)
         source_logits = tf.slice(c_logits, [0, 0], [self.batch_size, 2])
         target_logits = tf.slice(c_logits, [self.batch_size, 0], [self.batch_size, 2])
 
-        # ratio: in order to check if f_s approximate f_t. Here ratio should be smaller(0.02)
-        # delta_loss = tf.nn.l2_loss(source_logits)
-        # loss = tf.nn.l2_loss(source_feature)
-        # ratio = delta_loss / loss
-        ratio = 0
         merged_labels = tf.concat([source_labels, tf.one_hot(tf.argmax(tf.nn.softmax(target_logits), 1), 2)], 0)
         # merged_labels = tf.concat([source_labels, tf.nn.softmax(target_logits)], 0)
         # merge_features = self.kron(merge_features, 2, merged_labels, 50)
@@ -268,7 +153,7 @@ class DANN(object):
             differences = g_target_outputs - g_source_outputs
             interpolates = g_source_outputs + (alpha * differences)
             print(tf.shape(interpolates))  # [128, 4]
-            gradients = tf.gradients(self.discriminator(interpolates, 0, flag=1,
+            gradients = tf.gradients(self.discriminator(interpolates, None,
                                                         reuse=True), [interpolates])[0]
             # reduction_indices = 1, considering batch sample
             slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients), reduction_indices=[1]))
@@ -301,10 +186,7 @@ class DANN(object):
             c_train_op = tf.train.RMSPropOptimizer(lr).minimize(c_loss, step, c_vars)
 
         pack_op_loss = [d_train_op, g_train_op, c_train_op, d_loss, c_loss]
-        # pack_acc = [source_acc, target_acc, decay, lr,
-        #             ratio, clip_disc_weights, gradient_penalty, slopes]
-        pack_acc = [source_acc, target_acc, decay, lr,
-                    ratio, clip_disc_weights]
+        pack_acc = [source_acc, target_acc, decay, lr, clip_disc_weights]
         return pack_op_loss, pack_acc
 
     def train_model(self):
@@ -321,7 +203,7 @@ class DANN(object):
             pack_op_loss, pack_acc = self.build_model(source_images, target_images,
                                                       source_labels, target_labels,
                                                       global_step, self.mode,
-                                                      self.is_dan, is_train=True)
+                                                      is_train=True)
 
             d_train_op = pack_op_loss[0]
             g_train_op = pack_op_loss[1]
@@ -334,8 +216,7 @@ class DANN(object):
             target_acc = pack_acc[1]
             decay = pack_acc[2]
             lr = pack_acc[3]
-            ratio = pack_acc[4]
-            clip_disc_weights = pack_acc[5]
+            clip_disc_weights = pack_acc[4]
             # gradient_penalty = pack_acc[6]
             # slopes = pack_acc[7]
 
@@ -352,15 +233,11 @@ class DANN(object):
             tf.train.start_queue_runners(sess=sess)
             for step in xrange(self.max_steps + 1):
                 start_time = time.time()
-                # print('gradient_penalty: ', sess.run(gradient_penalty))
                 _, _, _ = \
                     sess.run([d_train_op, g_train_op, c_train_op])
                 if clip_disc_weights is not None:
                     _ = sess.run(clip_disc_weights)
                 duration = time.time() - start_time
-                # assert not np.isnan(t_loss), 'Model diverged with loss = NaN'
-
-                # print('slopes: ', sess.run(tf.shape(slopes)))
                 if step % params.train_show == 0:
                     d_l, c_l = sess.run([d_loss, c_loss])
                     acc_s, acc_t = sess.run([source_acc, target_acc])
@@ -396,7 +273,7 @@ class DANN(object):
             target_acc = self.build_model(None, test_target_images,
                                           None, test_target_labels,
                                           global_step, self.mode,
-                                          self.is_dan, is_train=False)
+                                          is_train=False)
 
             saver = tf.train.Saver(tf.global_variables())
             config = tf.ConfigProto()
@@ -417,23 +294,17 @@ class DANN(object):
             threads = []
             for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
                 threads.extend(qr.create_threads(sess, coord=coord, daemon=True, start=True))
-            true_count = 0
+            total_acc = 0
             step = 0
             while not coord.should_stop():
-                predictions = sess.run(accuracy)
-                true_count += predictions
+                acc = sess.run(accuracy)
+                total_acc += acc
                 step += 1
-            # precision = true_count / step
-            # print('precision:', precision)
         except Exception as e:
-            print ('step: ', step)
-            precision = true_count / step
-            print('precision:', precision)
+            print ('num: ', step)
+            avg_acc = total_acc / step
+            print('precision:', avg_acc)
             coord.request_stop(e)
             coord.request_stop()
             coord.join(threads, stop_grace_period_secs=1)
-            return precision
-        # coord.request_stop()
-        # coord.join(threads, stop_grace_period_secs=1)
-        #
-        # return precision
+            return avg_acc
